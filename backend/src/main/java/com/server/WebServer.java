f59921a024f700ebf33639cf3db85d6a9e1bb290
Sidd <sidd.s.pai@gmail.com>
2025-03-23 17:01:41 -0600

    Added explicit network creation.

--- backend/src/main/java/com/server/WebServer.java
+++ backend/src/main/java/com/server/WebServer.java
@@ -43,7 +43,8 @@ public class WebServer extends WebSocketServer {
     private String heartBeatAddress;
     private final String myServerAddress;
 
-    private final String coordinatorAddress = "ws://172.18.0.3:9999"; //proxy to frontend 
+    // private final String coordinatorAddress = "ws://172.18.0.3:9999"; //proxy to frontend 
+    private final String coordinatorAddress = "ws://connection_coordinator:9999"; // This can be commented out in case it doesn't work.
     private WebSocketClient coordinatorConnection;
 
     public static final Map<Integer, String> serverIdToAddressMap = new HashMap<>();
