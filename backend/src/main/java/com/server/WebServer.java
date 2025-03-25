134f74a619827090d4d5ebce8cefe5af0f06616e
Jin Song <songjayw@gmail.com>
2025-03-25 11:53:33 -0600

    static hostnames

--- backend/src/main/java/com/server/WebServer.java
+++ backend/src/main/java/com/server/WebServer.java
@@ -35,15 +35,15 @@ public class WebServer extends WebSocketServer {
     private final ReplicationManager replicationManager; //ReplicationManager instance
     private boolean isPrimary; //Flag to indicate if this server is the primary server
     private static final Map<String, Integer> serverNameToIdMap = Map.of(
-        "backup_server_1:6001", 1,
-        "backup_server_2:7001", 2,
-        "backup_server_3:4001", 3,
-        "primary_server:5001", 4
+        "44.203.203.158 :6001", 1,
+        "18.204.230.185:7001", 2,
+        "3.86.91.77:4001", 3,
+        "44.203.49.143:5001", 4
     );
     private String heartBeatAddress;
     private final String myServerAddress;
 
-    private final String coordinatorAddress = "ws://172.20.0.8:9999"; //proxy to frontend 
+    private final String coordinatorAddress = "ws://44.206.0.115:9999"; //proxy to frontend 
     private WebSocketClient coordinatorConnection;
 
     public static final Map<Integer, String> serverIdToAddressMap = new HashMap<>();
