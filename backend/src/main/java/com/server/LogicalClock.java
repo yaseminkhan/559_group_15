819359c5ced28cb310b8be4d369c8f406e3398bf
Sidd <sidd.s.pai@gmail.com>
2025-03-27 12:24:09 -0600

    Updated logical clock code for correct getters and setters.

--- backend/src/main/java/com/server/LogicalClock.java
+++ backend/src/main/java/com/server/LogicalClock.java
@@ -3,38 +3,25 @@ package com.server;
 public class LogicalClock {
     private int time;
 
-    record Send(Object m, int sequenceNo) {
-    }
-
-    record Receive(Object m, int sequenceNo) {
-    }
-
     public LogicalClock() {
         time = 0;
     }
 
-    public void tick() {
+    public synchronized void setTime(int value) {
+        if (value < time)
+            throw new IllegalArgumentException("Logical time is monotonic.");
+        time = value;
+    }
+
+    public synchronized int getTime() {
+        return time;
+    }
+
+    public synchronized void tick() {
         ++time;
     }
 
-    // public void onEvent(Object event) {
-    //     switch (event) {
-    //         case Send(Object m, int sequenceNo) -> {
-    //             System.out.println("SEND " + m);
-    //         }
-    //         case Receive(Object m, int sequenceNo) -> {
-    //             System.out.println("RECEIVE " + m);
-    //         }
-    //         default -> System.out.println("Unexpected value.");
-    //     }
-    // }
-
-    // public static void main(String[] args) {
-    //     var clk = new LogicalClock();
-    //     var e1 = new Send("MESSAGE", 0);
-    //     var e2 = new Receive("MESSAGE 2", 9);
-
-    //     clk.onEvent(e1);
-    //     clk.onEvent(e2);
-    // }
+    public synchronized void onEvent() {
+        
+    }
 }
