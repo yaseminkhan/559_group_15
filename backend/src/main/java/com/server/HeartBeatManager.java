22429f1a7141e491073f8847db0ee1bc644c0636
Sidd <sidd.s.pai@gmail.com>
2025-03-28 08:59:11 -0600

    Changed heartbeat timeout (temp).

--- backend/src/main/java/com/server/HeartBeatManager.java
+++ backend/src/main/java/com/server/HeartBeatManager.java
@@ -18,7 +18,7 @@ public class HeartBeatManager {
     private final List<String> allServers; //List of all server addresses
     private List<String> allHBServers; //List of all server addresses
     private long lastHeartbeatTime = System.currentTimeMillis(); //Timestamp of last received heartbeat
-    public static final int HEARTBEAT_TIMEOUT = 1000; //Set server time out as 1 second
+    public static final int HEARTBEAT_TIMEOUT = 5_000; //Set server time out as 5 seconds.
     private final ConcurrentHashMap<String, Long> lastHeartbeats = new ConcurrentHashMap<>();
     private final LeaderElectionManager leaderElectionManager;
     private static final Map<String, Integer> serverNameToPortMap = Map.of(
