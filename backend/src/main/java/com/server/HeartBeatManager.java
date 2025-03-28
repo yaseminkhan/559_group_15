7226bff2caab899c40a605fcc65a077eb3bd90d8
Jin Song <songjayw@gmail.com>
2025-03-28 02:26:04 -0600

    f

--- backend/src/main/java/com/server/HeartBeatManager.java
+++ backend/src/main/java/com/server/HeartBeatManager.java
@@ -156,7 +156,7 @@ public class HeartBeatManager {
             String[] hostParts = parts[1].split(":"); // Split at ":"
             String serverName = hostParts[0]; // Get "primary_server"
             System.out.println("Server name: " + serverName);
-            cleanHost = parts[1];
+            cleanHost = serverName;
             System.out.println("cleanHost: " + cleanHost);
         } else {
             // Case 2: "primary_server:5001" (already in correct format)
