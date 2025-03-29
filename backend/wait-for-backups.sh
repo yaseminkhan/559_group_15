e004de7bd34df60953d65b2ea823d361c8e8a239
Jin Song <songjayw@gmail.com>
2025-03-29 02:29:27 -0600

    f

--- backend/wait-for-backups.sh
+++ backend/wait-for-backups.sh
@@ -2,6 +2,10 @@
 # wait-for-backups.sh
 
 echo "Waiting for backup servers and connection coordinator..."
+echo "BACKUP_SERVER_1_IP=$BACKUP_SERVER_1_IP"
+echo "BACKUP_SERVER_2_IP=$BACKUP_SERVER_2_IP"
+echo "BACKUP_SERVER_3_IP=$BACKUP_SERVER_3_IP"
+echo "COORDINATOR_IP=$COORDINATOR_IP"
 
 until nc -z $BACKUP_SERVER_1_IP 6001; do sleep 1; done
 until nc -z $BACKUP_SERVER_2_IP 7001; do sleep 1; done
