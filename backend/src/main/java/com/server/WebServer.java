0be05ba0e6ef58544cac97ef384edb2178073bf0
Manvi Juneja <manvijuneja@gmail.com>
2025-03-31 00:03:10 -0600

    username of frontend fix

--- backend/src/main/java/com/server/WebServer.java
+++ backend/src/main/java/com/server/WebServer.java
@@ -575,6 +575,7 @@ public class WebServer extends WebSocketServer {
                    ", Updated backend clock: " + updatedTime);
 
         chat = game.addMessage(chat); 
+        System.out.println("Handle Chat Request: " + game.getChatEvents());
         broadcastToGame(game, "/chat " + gameCode + " " + gson.toJson(chat));
     }
 
