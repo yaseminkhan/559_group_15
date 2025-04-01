d348ba090d6239a7e24676cd75a758b54dc42426
Jin Song <songjayw@gmail.com>
2025-03-31 22:43:28 -0600

    f

--- backend/wait-for-kafka.sh
+++ backend/wait-for-kafka.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 # wait-for-backups.sh
 
+set -e
 echo "Waiting for backup servers and connection coordinator..."
 echo "KAFKA_IP=$KAFKA_IP"
-echo "COORDINATOR_IP=$COORDINATOR_IP"
 
 until nc -z $KAFKA_IP 9092; do sleep 1; done
-until nc -z $COORDINATOR_IP 9999; do sleep 1; done
 
 echo "All dependencies ready. Starting primary..."
 exec "$@"
